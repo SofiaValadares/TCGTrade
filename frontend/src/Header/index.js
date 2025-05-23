47ba5616dc23fb0fc6fa0cfb7650ef5adc7c1c2a
SofiaValadares <svc2@cesar.school>
2025-05-23 15:22:26 -0300

    header

---
+++ frontend/src/Header/index.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <dic className="header">
+            <div className="header-content">
+                <h1>TCGTrade</h1>
+            </div>
+        </dic>
+    )
+}
+
+export default Header
